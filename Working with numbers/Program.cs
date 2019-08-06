2f468022f3ff1a82883039d82af3002e6291255c
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 14:01:57 +0100

    Add project files.

---
+++ Working with numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Working_with_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine( Math.Round(4.3) ); // round to nearest whole number - 4
+
+            Console.ReadLine();
+        }
+    }
+}
